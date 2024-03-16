7a5cbb48314f0d5acb600496d3934ad16386506c
Svik192 <148989696+Svik192@users.noreply.github.com>
2024-03-16 17:48:34 +0200

    pc_dz7 +

--- clean_folder/clean_folder/clean.py
+++ clean_folder/clean_folder/clean.py
@@ -25,39 +25,39 @@ unknown = set()
 extensions = set()
 
 FLAG_FILE = 1
-FLAG_ARCHIVES  = 2
+FLAG_ARCHIVES = 2
 
 list_folders = [
-        [Image, 'Image',FLAG_FILE], 
-        [Video_files, 'Video_files',FLAG_FILE], 
-        [Documents, 'Documents',FLAG_FILE], 
-        [Music, 'Music',FLAG_FILE], 
-        [Archives,'Archives',FLAG_ARCHIVES],
-        [Unknown_extensions, 'Other',FLAG_FILE],
-    ]
+    [Image, 'Image', FLAG_FILE],
+    [Video_files, 'Video_files', FLAG_FILE],
+    [Documents, 'Documents', FLAG_FILE],
+    [Music, 'Music', FLAG_FILE],
+    [Archives, 'Archives', FLAG_ARCHIVES],
+    [Unknown_extensions, 'Other', FLAG_FILE],
+]
 
 registered_extensions = {
     'JPEG': Image,
     'PNG': Image,
     'JPG': Image,
-    'SVG': Image,    
+    'SVG': Image,
 
     'AVI': Video_files,
     'MP4': Video_files,
     'MOV': Video_files,
-    'MKV': Video_files,  
+    'MKV': Video_files,
 
     'DOC': Documents,
     'DOCX': Documents,
     'TXT': Documents,
-    'PDF': Documents, 
+    'PDF': Documents,
     'XLSX': Documents,
-    'PPTX': Documents,   
+    'PPTX': Documents,
 
     'MP3': Music,
     'OGG': Music,
     'WAV': Music,
-    'AMR': Music,   
+    'AMR': Music,
 
     'ZIP': Archives,
     'GZ': Archives,
@@ -65,8 +65,10 @@ registered_extensions = {
 }
 
 UKRAINIAN_SYMBOLS = 'абвгдеєжзиіїйклмнопрстуфхцчшщьюя'
-TRANSLATION = ("a", "b", "v", "g", "d", "e", "je", "zh", "z", "y", "i", "ji", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
-               "f", "h", "ts", "ch", "sh", "sch", "", "ju", "ja")
+TRANSLATION = (
+    "a", "b", "v", "g", "d", "e", "je", "zh", "z", "y", "i", "ji", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t",
+    "u",
+    "f", "h", "ts", "ch", "sh", "sch", "", "ju", "ja")
 
 TRANS = {}
 
@@ -74,14 +76,17 @@ for key, value in zip(UKRAINIAN_SYMBOLS, TRANSLATION):
     TRANS[ord(key)] = value
     TRANS[ord(key.upper())] = value.upper()
 
+
 def normalize(name: str) -> str:
     name, *extension = name.split('.')
     new_name = name.translate(TRANS)
     new_name = re.sub(r'\W', '_', new_name)
     return f"{new_name}.{'.'.join(extension)}"
 
+
 def get_extensions(file_name):
     return Path(file_name).suffix[1:].upper()
+
 
 def scan(folder):
     folders = list()
@@ -89,7 +94,7 @@ def scan(folder):
         folders.append(params[1])
 
     for item in folder.iterdir():
-        if item.is_dir():       
+        if item.is_dir():
             # if item.name not in ('Image', 'Video_files', 'Documents', 'Music', 'Archives', 'Other'):
             if item.name not in folders:
                 folders.append(item)
@@ -97,7 +102,7 @@ def scan(folder):
             continue
 
         extension = get_extensions(file_name=item.name)
-        new_name = folder/item.name
+        new_name = folder / item.name
         if not extension:
             Unknown_extensions.append(new_name)
         else:
@@ -111,9 +116,10 @@ def scan(folder):
 
 
 def handle_file(path, root_folder, dist):
-    target_folder = root_folder/dist
+    target_folder = root_folder / dist
     target_folder.mkdir(exist_ok=True)
-    path.replace(target_folder/normalize(path.name))
+    path.replace(target_folder / normalize(path.name))
+
 
 def handle_archive(path, root_folder, dist):
     target_folder = root_folder / dist
@@ -144,21 +150,21 @@ def remove_empty_folders(path):
             except OSError:
                 pass
 
+
 def print_list(list):
     res_str = "\n"
     for el in list:
         res_str += f"\t{el}\n"
 
     return res_str
-        
+
+
 def write_to_file():
-
-    with open(str(Path(sys.argv[1]))+"\\list.txt", 'w') as fh:
-
+    with open(str(Path(sys.argv[1])) + "\\list.txt", 'w') as fh:
         for params in list_folders:
             fh.write(f"{params[1]}: {print_list(params[0])}\n")
 
-        #fh.write(f"Folder: {folders}\n")
+        # fh.write(f"Folder: {folders}\n")
 
         fh.write(f"All extensions: {print_list(extensions)}\n")
         fh.write(f"Unknown extensions: {print_list(unknown)}\n")
@@ -169,41 +175,24 @@ def main():
     print(folder_path)
     scan(folder_path)
 
-    for params in list_folders:
-        print(f"{params[1]}: {print_list(params[0])}")
+    # list_folders = [
+    #     [Image, 'Image', FLAG_FILE],
+    #       ...
+    for list_file, name_folder, flag in list_folders:
+        print(f"{name_folder}: {print_list(list_file)}")
 
-        for file in params[0]:
-            if params[2] == FLAG_FILE:
-                handle_file(file, folder_path, params[1])
-            elif params[2] == FLAG_ARCHIVES:
-                handle_archive(file, folder_path, params[1])
-        
+        for file in list_file:
+            if flag == FLAG_FILE:
+                handle_file(file, folder_path, name_folder)
+            elif flag == FLAG_ARCHIVES:
+                handle_archive(file, folder_path, name_folder)
+
     remove_empty_folders(folder_path)
 
     write_to_file()
 
     print(f"Done")
 
+
 if __name__ == '__main__':
     main()
-
-
-    # for file in Image:
-    #     handle_file(file, folder_path, "Image")
-
-    # for file in Video_files:
-    #     handle_file(file, folder_path, "Video_files")
-
-    # for file in Documents:
-    #     handle_file(file, folder_path, "Documents")
-
-    # for file in Music:
-    #     handle_file(file, folder_path, "Music")
-
-    # for file in Unknown_extensions:
-    #     handle_file(file, folder_path, "Unknown_extensions")
-
-    # for file in Archives:
-    #     handle_archive(file, folder_path, "Unpack_Archive")
-
-
